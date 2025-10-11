b8333cec4b411721c90758b272a59f7816412446
geeksguildaustralia <geeksguildaustralia@gmail.com>
2025-10-11 11:54:05 +1100

    Update: remoe and from file name

--- build_set_index.js
+++ build_set_index.js
@@ -23,6 +23,7 @@ function normalizeName(name) {
   return name
     .toLowerCase()
     .replace(/&/g, 'and')
+    .replace(/\band\b/g, '')
     .replace(/'/g, '')
     .replace(/[^a-z0-9]/g, '-')
     .replace(/-+/g, '-')
